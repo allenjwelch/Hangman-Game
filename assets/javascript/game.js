dcad5ecc343faabeff0bf76abd27194c64074f06
Allen Welch <allenjwelch@gmail.com>
2018-02-26 11:26:40 -0500

    resolved output issue 2.26

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,6 @@ var wordInPlay = wordInPlay.split("");
   console.log(wordInPlay);
 
 var output = []; 
-
   for (var i = 0; i < wordInPlay.length; i++) {
     output.push("__")
   } console.log(output); 
@@ -21,7 +20,7 @@ var userGuessRemaining = 6;
     
 var lettersGuessed = []; 
 
-var correctLetter = false;
+var correctLetter;
 
 
 
@@ -29,52 +28,107 @@ var correctLetter = false;
 
 
 // 3. GAME START
-document.onkeydown = function(event) {
-  var userGuess = event.key;
-  userGuess = userGuess.toUpperCase(); 
-  console.log(userGuess); 
-  lettersGuessed.push(userGuess); 
+// function gamePlay() {
+  document.onkeydown = function(event) {
+    userGuess = event.key;
+    userGuess = userGuess.toUpperCase(); 
+    console.log('userGuess:', userGuess); 
+    lettersGuessed.push(userGuess); 
 
 
-// ALL GAME CODE GOES IN THIS FUNCTION
+    // ALL GAME CODE GOES IN THIS FUNCTION
 
-//Check if userGuess matches wordInPlay
-  
-  
-function checkLetter(userGuess) {
-  // for (i = 0; i < wordInPlay.length; i++) {
-  
-    if (wordInPlay.indexOf(userGuess) > -1) {
-      console.log('MATCH!'); 
-      output[wordInPlay.indexOf(userGuess)] = userGuess; 
-    } else {
-      console.log('NO match'); 
-      userGuessRemaining--; 
-    } 
-  // }
+    //Check if userGuess matches wordInPlay 
+    function checkLetter() {
+      //First, checks to see if there is a match
+      if (wordInPlay.includes(userGuess)) {
+        correctLetter = true; 
+        console.log('MATCH!'); 
+        //If a match was found FOR loop begins to loops through all letters
+        for (i = 0; i < wordInPlay.length; i++) {
+          console.log(i); 
+          //As the FOR loop cycles through all letters, each time a value in the array matches the userGuess the output array updates with the same value and index
+          if (wordInPlay[i] === userGuess) {
+            output[i] = wordInPlay[i];
+          } else {
+            console.log("This is exiting the nested IF loop"); 
+          }
+        }; 
+      } else {
+        correctLetter = false; 
+        console.log('NO match');
+        userGuessRemaining--; 
+      } 
+    }; //Close checkLetter function
+
+    //////////////////////////SCRAP HEAP///////////////////
+      // if (wordInPlay.indexOf(userGuess) > -1) {
+      //   console.log('MATCH!'); 
+      //   var newIndex = wordInPlay.indexOf(userGuess)
+      //   console.log(newIndex);
+      //   output[wordInPlay.indexOf(userGuess)] = userGuess; 
+        
+      // } else {
+      //   console.log('NO match'); 
+      //   userGuessRemaining--; 
+      //   } 
+
+      // if (wordInPlay[i] === userGuess) {
+      //   correctLetter = true; 
+      //   console.log('MATCH!'); 
+      //   output[wordInPlay.indexOf(userGuess)] = userGuess; 
+      //   wordInPlay[userGuess] = 'X'; 
+      // } else {
+      //   correctLetter = false; 
+      //   console.log('NO match');
+      // }
+
+      function checkWin() {
+        if (output.indexOf('__') < 0) {
+          console.log('WINNER!');
+        } else if (userGuessRemaining == 0) {
+          console.log('LOSER!');
+        } else {
+          console.log('idk!');
+        }
+      };
+    
+    //   function addClown() {
+    //     switch (userGuessRemaining) {
+    //       case 5:
+    //         document.querySelector('#clownHead').style.visibility = "visibile";
+    //         break; 
+    //       case 4:
+    //         document.querySelector('#clownBody').style.visibility = "visibile";
+    //         break; 
+    //       case 3:
+    //         document.querySelector('#clownHead').style.visibility = "visibile";
+    //         break; 
+    //   }
+    // }
 
 
-}
 
-  // if (wordInPlay.includes(userGuess)) {
+    
+    checkLetter(userGuess); 
+    checkWin(); 
+    console.log('lettersGuessed:', lettersGuessed);
+    console.log('userGuessRemaining:', userGuessRemaining); 
 
-
-  checkLetter(userGuess); 
-  console.log('lettersGuessed:', lettersGuessed);
-  console.log('userGuessRemaining:', userGuessRemaining); 
-
-  ///////////////////// DISPLAYS INPUT ON SCREEN/////////////////////
-  document.getElementById("wordInPlay").innerHTML = wordInPlay;
-  document.getElementById("output").innerHTML = output; 
-  document.getElementById("guess").innerHTML = lettersGuessed; 
-  document.getElementById("userGuessRemaining").innerHTML = userGuessRemaining; 
+    ///////////////////// DISPLAYS INPUT ON SCREEN/////////////////////
+    document.getElementById("wordInPlay").innerHTML = wordInPlay;
+    document.getElementById("output").innerHTML = output.join(' '); 
+    document.getElementById("guess").innerHTML = lettersGuessed.join(' '); 
+    document.getElementById("userGuessRemaining").innerHTML = userGuessRemaining; 
 
 
 
 
 
 
-};
+  }; //End document.event
+
+// }; //End gamePlay
 
 
 
